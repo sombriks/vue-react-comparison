1d4ef57b2c6bd068ed463358f726e1de993af55e
sombriks <sombriks@gmail.com>
2018-12-29 14:02:58 -0300

    updating things

--- my-vue-project-2/main.js
+++ my-vue-project-2/main.js
@@ -1,4 +1,4 @@
 const Vue = require("vue")
-const app = document.getElementById("div")
+const app = document.createElement("div")
 document.body.appendChild(app)
 new Vue({el:app, render: r => r(require("./app.vue"))})
